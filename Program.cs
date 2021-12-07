3bb150bfa9d11de3f2a1fe1cd0e110b823579292
Fredrik Lam <fredrik.didi.lam@gmail.com>
2021-12-07 21:22:51 +0100

    Complete Part2

--- Program.cs
+++ Program.cs
@@ -38,14 +38,40 @@ namespace _2
         {
             string[] textFile = File.ReadAllLines(@"C:\Users\fredd\OneDrive\.NET Developer\AdventOfCode\2\input.txt");
 
-            //int horizontal = 0;
-            //int depth = 0;
-            //int aim = 0;
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+
+            foreach (var text in textFile)
+            {
+                if (text.Contains(" "))
+                {
+                    string direction = text.Split(' ')[0];
+                    int step = int.Parse(text.Split(' ')[1]);
+
+                    if (direction == "forward")
+                    {
+                        horizontal += step;
+                        depth += aim * step;
+                    }
+                    else if (direction == "up")
+                    {
+                        aim -= step;
+                    }
+                    else if (direction == "down")
+                    {
+                        aim += step;
+                    }
+                }
+            }
+            Console.WriteLine(depth * horizontal);
         }
 
         static void Main(string[] args)
         {
             PartOne();
+            Console.WriteLine();
+            PartTwo();
 
             //forward + (horisontal)
             //up -      (depth)
@@ -57,6 +83,14 @@ namespace _2
             ----------------------
             (up, steg) /\
             (down, steg) \/
+            */
+
+            /*
+            down X increases your aim by X units.       down aim +X
+            up X decreases your aim by X units.         up aim -X
+            forward X does two things:
+            It increases your horizontal position by X units.   horizontal +X
+            It increases your depth by your aim multiplied by X.        depth = aim * X
             */
         }
     }
