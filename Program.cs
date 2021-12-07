99a770fe94b005550836303ec3d4b0375bf08e7a
Fredrik Lam <fredrik.didi.lam@gmail.com>
2021-12-07 20:58:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace _2
+{
+    class Program
+    {
+        public static void PartOne()
+        {
+            string[] textFile = File.ReadAllLines(@"C:\Users\fredd\OneDrive\.NET Developer\AdventOfCode\2\input.txt");
+
+            int horizontal = 0;
+            int depth = 0;
+            foreach (var text in textFile)
+            {
+                if (text.Contains(" "))
+                {
+                    string direction = text.Split(' ')[0];
+                    int step = int.Parse(text.Split(' ')[1]);
+
+                    if (direction == "forward")
+                    {
+                        horizontal += step;
+                    }
+                    else if (direction == "up")
+                    {
+                        depth -= step;
+                    }
+                    else if (direction == "down")
+                    {
+                        depth += step;
+                    }
+                }
+            }
+            Console.WriteLine(depth * horizontal);
+        }
+
+        static void Main(string[] args)
+        {
+            //forward + (horisontal)
+            //up -      (depth)
+            //down +    (depth)
+
+
+            /*
+            (forward, steg) ->
+            ----------------------
+            (up, steg) /\
+            (down, steg) \/
+            */
+        }
+    }
+}
+
+
+
