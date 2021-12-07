366312857ba6b7f01c452c9dbcdf0d12a20d8e8f
Fredrik Lam <fredrik.didi.lam@gmail.com>
2021-12-07 21:07:04 +0100

    Created PartTwo Method
    Only beginning

--- Program.cs
+++ Program.cs
@@ -34,9 +34,19 @@ namespace _2
             }
             Console.WriteLine(depth * horizontal);
         }
+        public static void PartTwo()
+        {
+            string[] textFile = File.ReadAllLines(@"C:\Users\fredd\OneDrive\.NET Developer\AdventOfCode\2\input.txt");
+
+            //int horizontal = 0;
+            //int depth = 0;
+            //int aim = 0;
+        }
 
         static void Main(string[] args)
         {
+            PartOne();
+
             //forward + (horisontal)
             //up -      (depth)
             //down +    (depth)
